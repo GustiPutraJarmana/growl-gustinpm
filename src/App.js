9d23ba5f1928007d75191ab6301124f597ecb1d7
Gusti Putra <gustiputra@Gustis-MacBook-Pro.local>
2023-01-17 00:52:04 +1100

    gustiPutra/JadenDigital/[03]-fixing dependeces

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 
-import './growl.css'
-
-export const App = ({ active, message, onDismissed }) => (
-  <div className={`growl${active ? "active" : ""}`}>
-    {message}
-    <div onClick={onDismissed} className="growl-close" />
-  </div>
-)
-
-export function useGrowl(timeOut) {
-    // state of the growl
-    const [growlActive, setGrowlActive] = React.useState(false)
-
-    useEffect(() => {
-      if (growlActive) {
-        setTimeout(() => {
-          setGrowlActive(false)
-        }, timeOut ? timeOut : 3000)
-      }
-    }, [growlActive, timeOut])
-
-    return [
-        // the first arg is the state
-        growlActive, 
-
-        // the second arg is a fn that allows you to safely set its state
-        (active) => {
-            setGrowlActive(active)
-        },
-    ]
+const App = () => {
+  return (
+    <>app</>
+  )
 }
+
+export default App
---
+++ src/components/growl.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react'
+
+import './growl.css'
+
+export const Growl = ({ active, message, onDismissed }) => (
+  <div className={`growl${active ? " active" : ""}`}>
+    {message}
+    <div onClick={onDismissed} className="growl-close" />
+  </div>
+)
+
+export function useGrowl(timeOut) {
+    // state of the growl
+    const [growlActive, setGrowlActive] = useState(false)
+
+    useEffect(() => {
+      if (growlActive) {
+        setTimeout(() => {
+          setGrowlActive(false)
+        }, timeOut ? timeOut : 3000)
+      }
+    }, [growlActive, timeOut])
+
+    return [
+        // the first arg is the state
+        growlActive, 
+
+        // the second arg is a fn that allows you to safely set its state
+        (active) => {
+            setGrowlActive(active)
+        },
+    ]
+}
